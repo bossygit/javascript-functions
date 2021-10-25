9b123b1a99e1abfa8fa3c669a31cf93159e46503
bossygit <bienvenu.kitutu@gmail.com>
2021-10-25 20:46:13 +0100

    same function

--- js/gameoflife.js
+++ js/gameoflife.js
@@ -6,7 +6,11 @@ function seed() {
   return retour;
 }
 
-function same([x, y], [j, k]) { }
+function same([x, y], [j, k]) {
+
+  if (x == j && y == k) return true;
+  else return false;
+}
 
 // The game state to search for `cell` is passed as the `this` value of the function.
 function contains(cell) { }
